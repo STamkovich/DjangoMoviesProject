431c890e3542019842aa050af8b71d8ec2bc73cc
STamkovich <tamkovich1072@gmail.com>
2022-04-22 17:37:42 +0300

    "Создания моделей"

--- movies/models.py
+++ movies/models.py
@@ -1,11 +1,12 @@
 from django.db import models
+from datetime import date
 
 
 class Category(models.Model):
     """Категории"""
     name = models.CharField("Категория", max_length=150)
     description = models.TextField("Описание")
-    url = models.SlugField(max_length=160)
+    url = models.SlugField(max_length=160, unique=True)  # unique=True  указывает что она уникальная
 
     def __str__(self):
         return f'{self.name}'
@@ -14,3 +15,67 @@ class Category(models.Model):
 class Meta:
     verbose_name = "Категория"
     verbose_name_plural = "Категории"
+
+
+class Actor(models.Model):
+    """Актёры и режиссёры"""
+    name = models.CharField("Имя", max_length=100)
+    age = models.PositiveSmallIntegerField("Возраст", default=0)
+    description = models.TextField("Описание")
+    image = models.ImageField("Изображение", upload_to="actors/")  # upload_to="actors/" указывает деректорию
+
+    # куда будет загружено изображение
+
+    def __str__(self):
+        return f'{self.name}'
+
+    class Meta:
+        verbose_name = "Актёры и режиссёры"
+        verbose_name_plural = "Актёры и режиссёры"
+
+
+class Genre(models.Model):
+    """Жанры"""
+    name = models.CharField("Имя", max_length=100)
+    description = models.TextField("Описание")
+    url = models.SlugField(max_length=160, unique=True)  # unique=True указывает что она уникальная
+
+    def __str__(self):
+        return f'{self.name}'
+
+    class Meta:
+        verbose_name = "Жанр"
+        verbose_name_plural = "Жанры"
+
+
+class Movie(models.Model):
+    """Фильмы"""
+    title = models.CharField("Название", max_length=100)
+    tagline = models.CharField("Слоган", max_length=100, default='')
+    description = models.TextField("Описание")
+    poster = models.ImageField("Постер", upload_to="movies/")
+    year = models.PositiveSmallIntegerField("Дата выхода", default=2019)
+    country = models.CharField("Страна", max_length=30)
+    directors = models.ManyToManyField(Actor, verbose_name="режиссёр", related_name="film_director")
+    actors = models.ManyToManyField(Actor, verbose_name="актёры", related_name="film_actor")
+    genres = models.ManyToManyField(Genre, verbose_name="жанры")
+    world_premiere = models.DateField("Премьера в мире", default=date.today)
+    budget = models.PositiveIntegerField("Бюджет", default=0, help_text="Указывать сумму в долларах")
+    fees_in_usa = models.PositiveIntegerField(
+        "Сборы в США", default=0, help_text="Указывать сумму в долларах"
+    )
+    fees_in_world = models.PositiveIntegerField(
+        "Сборы в мире", default=0, help_text="Указывать сумму в долларах"
+    )
+    category = models.ForeignKey(
+        Category, verbose_name="Категории", on_delete=models.SET_NULL, null=True
+    )
+    url = models.SlugField(max_length=160, unique=True)
+    draft = models.BooleanField("Черновик", default=False)
+
+    def __str__(self):
+        return f'{self.title}'
+
+    class Meta:
+        verbose_name = "Фильм"
+        verbose_name_plural = "Фильмы"
