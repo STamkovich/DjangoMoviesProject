0ea9f7ffae15a1323036218d202cbaefda5b821f
STamkovich <tamkovich1072@gmail.com>
2022-04-22 17:02:24 +0300

    Initial commit

---
+++ movies/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+
+class Category(models.Model):
+    """Категории"""
+    name = models.CharField("Категория", max_length=150)
+    description = models.TextField("Описание")
+    url = models.SlugField(max_length=160)
+
+    def __str__(self):
+        return f'{self.name}'
+
+
+class Meta:
+    verbose_name = "Категория"
+    verbose_name_plural = "Категории"
